9c2d0922aaa54dc6d17992842d10e3ee15032cdc
JenisRajan <jenisrajan007@gmail.com>
2024-05-12 22:21:16 +0530

    Update updateddashboard.py

--- updateddashboard.py
+++ updateddashboard.py
@@ -1,38 +1,39 @@
-
+# Importing the necessary packages
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Reading CSV files
+# Reading the requiresd CSV files
 orders_data = pd.read_csv('orders_cleaned.csv')
 rules_data = pd.read_csv('association_rules_results.csv')
 
+#Setting the title and layout for the dashboard in streamlit
 st.set_page_config(layout='wide')
 st.title(':department_store: Minger Electronics Insights Dashboard')
 col1, col2 = st.columns((2))
-# Adding dashboard filter
+# Adding the  dashboard filter
 st.sidebar.title("Dashboard Filters")
+# Making seperate tabs for the insights
+tab = st.sidebar.radio("Navigation", ('Order Insights', 'Market Basket Insights'))
 
-# Making tabs
-tab = st.sidebar.radio("Navigation", ('Order Details', 'Market Basket Analysis'))
-
-if tab == 'Order Details':
+if tab == 'Order Insights':
     st.header("Order Details")
-    st.write(orders_data)
-    # Picking the date 
+    
+    # Picking the starting and ending date 
     orders_data['Order Date'] = pd.to_datetime(orders_data['Order Date'])
     start_date = pd.to_datetime(orders_data['Order Date']).min()
     end_date = pd.to_datetime(orders_data['Order Date']).max()
-
-    start = pd.to_datetime(st.sidebar.date_input('Pick start date', start_date))
-    end = pd.to_datetime(st.sidebar.date_input('Pick end date', end_date))
+    
+    #Adding them to the layout
+    start = pd.to_datetime(st.sidebar.date_input('Select Start Date', start_date))
+    end = pd.to_datetime(st.sidebar.date_input('Select End Date', end_date))
     orders_data = orders_data[(orders_data['Order Date'] >= start) & (orders_data['Order Date'] <= end)].copy()
 
-    # Product by category and market
-    market = st.sidebar.selectbox('Pick your Market', orders_data['Market'].unique())
-    category = st.sidebar.multiselect('Pick your category', orders_data['Category'].unique())
+    # Product by "Category" and "Market"
+    market = st.sidebar.selectbox('Choose Your Market', orders_data['Market'].unique())
+    category = st.sidebar.multiselect('Choose your Category', orders_data['Category'].unique())
 
     # Filtering the dashboard using the "Market" and "Product" category
     if market and category:
@@ -47,50 +48,49 @@ if tab == 'Order Details':
     else:
         filtered_data = orders_data.copy()
 
-    # Charts for the Orders Dataset
+    # Charts for the "Order's Insight"
+
+    # Sales Over Time
+    st.subheader('Sales over time')
+    filtered_data.loc[:, "month_year"] = filtered_data["Order Date"].dt.to_period("M")
+    line_chart = pd.DataFrame(filtered_data.groupby(filtered_data["month_year"].dt.strftime("%Y : %b"))["Sales"].sum()).reset_index()
+    line_chart = line_chart.sort_values(by="month_year")
+    fig_sales_over_time = px.line(line_chart, x="month_year", y="Sales", labels={"Sales": "Amount"}, height=500, width=1500, template="gridon")
+    st.plotly_chart(fig_sales_over_time, use_container_width=True)
 
     # Sales by Sub Category
     st.subheader('Sales by Sub Category')
     grp = filtered_data.groupby(by=['Sub-Category'], as_index=False)['Sales'].sum()
-    fig1 = px.bar(grp, x='Sub-Category', y='Sales', height=600, width=700)
-    st.plotly_chart(fig1, use_container_width=True)
-
-    # Sales by Ship Mode
-    st.subheader('Sales by Ship Mode')
-    fig2 = px.box(filtered_data, x='Ship Mode', y='Sales', height=400, width=600)
-    st.plotly_chart(fig2, use_container_width=True)
+    fig_sales_sub_category = px.bar(grp, x='Sub-Category', y='Sales', height=600, width=700)
+    st.plotly_chart(fig_sales_sub_category, use_container_width=True)
 
     # Profit by Country
     st.subheader('Profit by Country')
     grp = filtered_data.groupby(by=['Country'], as_index=False)['Profit'].sum()
-    fig3 = px.bar(grp, x="Country", y="Profit")
-    st.plotly_chart(fig3, use_container_width=True)
+    fig_profit_by_country = px.bar(grp, x="Country", y="Profit")
+    st.plotly_chart(fig_profit_by_country, use_container_width=True)
+
+    # Sales by Ship Mode
+    st.subheader('Sales by Ship Mode')
+    fig_sales_ship_mode = px.box(filtered_data, x='Ship Mode', y='Sales', height=400, width=600)
+    st.plotly_chart(fig_sales_ship_mode, use_container_width=True)
 
     chart1, chart2 = st.columns((2))
     with chart1:
-        # Scatter plot to show relationship between profit and sales
-        scatter = px.scatter(orders_data, x="Quantity", y="Profit", size='Sales')
-        scatter['layout'].update(title="Relationship between Sales and Profits using Scatter Plot.",
-                                 titlefont=dict(size=20), xaxis=dict(title="Sales", titlefont=dict(size=19)),
-                                 yaxis=dict(title="Profit", titlefont=dict(size=19)))
-        st.plotly_chart(scatter, use_container_width=True)
-
-    with chart2:
         # Segment wise sales distribution
         st.subheader('Segment wise profit distribution')
+        fig_segment_profit_distribution = px.pie(orders_data, values="Sales", names='Segment')
+        fig_segment_profit_distribution.update_traces(text=orders_data['Segment'], textposition='outside')
+        st.plotly_chart(fig_segment_profit_distribution, use_container_width=True)
 
-        fig5 = px.pie(orders_data, values="Sales", names='Segment')
-        fig5.update_traces(text=orders_data['Segment'], textposition='outside')
-        st.plotly_chart(fig5, use_container_width=True)
+    with chart2:
+        # Scatter plot to show relationship between profit and sales
+        scatter_plot = px.scatter(orders_data, x="Quantity", y="Profit", size='Sales')
+        scatter_plot['layout'].update(title="Relationship between Sales and Profits using Scatter Plot.",
+                                 titlefont=dict(size=20), xaxis=dict(title="Sales", titlefont=dict(size=19)),
+                                 yaxis=dict(title="Profit", titlefont=dict(size=19)))
+        st.plotly_chart(scatter_plot, use_container_width=True)
 
-    # Line chart to show sales over time
-    filtered_data.loc[:, "month_year"] = filtered_data["Order Date"].dt.to_period("M")
-    st.subheader('Sales over time')
-
-    line = pd.DataFrame(filtered_data.groupby(filtered_data["month_year"].dt.strftime("%Y : %b"))["Sales"].sum()).reset_index()
-    line = line.sort_values(by="month_year")
-    fig4 = px.line(line, x="month_year", y="Sales", labels={"Sales": "Amount"}, height=500, width=1500, template="gridon")
-    st.plotly_chart(fig4, use_container_width=True)
 
 else:
     st.header("Market Basket Analysis Association Rules")
