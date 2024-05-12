36ec798c5626e6ee721b5155b966a9a0c869fc03
JenisRajan <jenisrajan007@gmail.com>
2024-05-12 22:49:05 +0530

    Update updateddashboard.py

--- updateddashboard.py
+++ updateddashboard.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -20,7 +19,7 @@ tab = st.sidebar.radio("Navigation", ('Order Details', 'Market Basket Analysis')
 
 if tab == 'Order Details':
     st.header("Order Details")
-    st.write(orders_data)
+    
     # Picking the date 
     orders_data['Order Date'] = pd.to_datetime(orders_data['Order Date'])
     start_date = pd.to_datetime(orders_data['Order Date']).min()
@@ -49,48 +48,46 @@ if tab == 'Order Details':
 
     # Charts for the Orders Dataset
 
+    # Sales Over Time
+    st.subheader('Sales over time')
+    filtered_data.loc[:, "month_year"] = filtered_data["Order Date"].dt.to_period("M")
+    line_chart = pd.DataFrame(filtered_data.groupby(filtered_data["month_year"].dt.strftime("%Y : %b"))["Sales"].sum()).reset_index()
+    line_chart = line_chart.sort_values(by="month_year")
+    fig_sales_over_time = px.line(line_chart, x="month_year", y="Sales", labels={"Sales": "Amount"}, height=500, width=1500, template="gridon")
+    st.plotly_chart(fig_sales_over_time, use_container_width=True)
+
     # Sales by Sub Category
     st.subheader('Sales by Sub Category')
     grp = filtered_data.groupby(by=['Sub-Category'], as_index=False)['Sales'].sum()
-    fig1 = px.bar(grp, x='Sub-Category', y='Sales', height=600, width=700)
-    st.plotly_chart(fig1, use_container_width=True)
-
-    # Sales by Ship Mode
-    st.subheader('Sales by Ship Mode')
-    fig2 = px.box(filtered_data, x='Ship Mode', y='Sales', height=400, width=600)
-    st.plotly_chart(fig2, use_container_width=True)
+    fig_sales_sub_category = px.bar(grp, x='Sub-Category', y='Sales', height=600, width=700)
+    st.plotly_chart(fig_sales_sub_category, use_container_width=True)
 
     # Profit by Country
     st.subheader('Profit by Country')
     grp = filtered_data.groupby(by=['Country'], as_index=False)['Profit'].sum()
-    fig3 = px.bar(grp, x="Country", y="Profit")
-    st.plotly_chart(fig3, use_container_width=True)
+    fig_profit_by_country = px.bar(grp, x="Country", y="Profit")
+    st.plotly_chart(fig_profit_by_country, use_container_width=True)
+
+    # Sales by Ship Mode
+    st.subheader('Sales by Ship Mode')
+    fig_sales_ship_mode = px.box(filtered_data, x='Ship Mode', y='Sales', height=400, width=600)
+    st.plotly_chart(fig_sales_ship_mode, use_container_width=True)
 
     chart1, chart2 = st.columns((2))
     with chart1:
-        # Scatter plot to show relationship between profit and sales
-        scatter = px.scatter(orders_data, x="Quantity", y="Profit", size='Sales')
-        scatter['layout'].update(title="Relationship between Sales and Profits using Scatter Plot.",
-                                 titlefont=dict(size=20), xaxis=dict(title="Sales", titlefont=dict(size=19)),
-                                 yaxis=dict(title="Profit", titlefont=dict(size=19)))
-        st.plotly_chart(scatter, use_container_width=True)
-
-    with chart2:
         # Segment wise sales distribution
         st.subheader('Segment wise profit distribution')
+        fig_segment_profit_distribution = px.pie(orders_data, values="Sales", names='Segment')
+        fig_segment_profit_distribution.update_traces(text=orders_data['Segment'], textposition='outside')
+        st.plotly_chart(fig_segment_profit_distribution, use_container_width=True)
 
-        fig5 = px.pie(orders_data, values="Sales", names='Segment')
-        fig5.update_traces(text=orders_data['Segment'], textposition='outside')
-        st.plotly_chart(fig5, use_container_width=True)
-
-    # Line chart to show sales over time
-    filtered_data.loc[:, "month_year"] = filtered_data["Order Date"].dt.to_period("M")
-    st.subheader('Sales over time')
-
-    line = pd.DataFrame(filtered_data.groupby(filtered_data["month_year"].dt.strftime("%Y : %b"))["Sales"].sum()).reset_index()
-    line = line.sort_values(by="month_year")
-    fig4 = px.line(line, x="month_year", y="Sales", labels={"Sales": "Amount"}, height=500, width=1500, template="gridon")
-    st.plotly_chart(fig4, use_container_width=True)
+    with chart2:
+        # Scatter plot to show relationship between profit and sales
+        scatter_plot = px.scatter(orders_data, x="Quantity", y="Profit", size='Sales')
+        scatter_plot['layout'].update(title="Relationship between Sales and Profits using Scatter Plot.",
+                                 titlefont=dict(size=20), xaxis=dict(title="Sales", titlefont=dict(size=19)),
+                                 yaxis=dict(title="Profit", titlefont=dict(size=19)))
+        st.plotly_chart(scatter_plot, use_container_width=True)
 
 else:
     st.header("Market Basket Analysis Association Rules")
@@ -106,15 +103,15 @@ else:
 
     chart1, chart2 = st.columns((2))
     with chart1:
-        fig6 = px.bar(rules_data, x='support', y='antecedents', orientation='h', title='Top Antecedents based on Support')
-        st.plotly_chart(fig6, use_container_width=True)
+        fig_top_antecedents = px.bar(rules_data, x='support', y='antecedents', orientation='h', title='Top Antecedents based on Support')
+        st.plotly_chart(fig_top_antecedents, use_container_width=True)
     with chart2:
-        fig7 = px.bar(rules_data, x='support', y='consequents', orientation='h', title='Top Consequents based on Support')
-        st.plotly_chart(fig7, use_container_width=True)
+        fig_top_consequents = px.bar(rules_data, x='support', y='consequents', orientation='h', title='Top Consequents based on Support')
+        st.plotly_chart(fig_top_consequents, use_container_width=True)
 
     # The Treemap
     st.subheader("Hierarchical view of Antecedents with their Consequents based Support")
-    fig10 = px.treemap(rules_data, path=["antecedents", "consequents"], values="support", hover_data=["support"],
+    fig_hierarchical_view = px.treemap(rules_data, path=["antecedents", "consequents"], values="support", hover_data=["support"],
                        color="consequents")
-    fig10.update_layout(width=800, height=650)
-    st.plotly_chart(fig10, use_container_width=True)
+    fig_hierarchical_view.update_layout(width=800, height=650)
+    st.plotly_chart(fig_hierarchical_view, use_container_width=True)
